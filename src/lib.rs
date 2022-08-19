4778bcd169ff3c6fc6dfa2e3a16a37db705b0d11
FIN4LSHAR3 <60556855+FinalSh4re@users.noreply.github.com>
2022-08-20 01:43:29 +0200

    Refactor: Change CommandPipeline from tuple struct to regular one

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 //! A simple annonymous UNIX pipe type.
 //!
 //! ## Usage
-//! 
+//!
 //! ```rust
 //! use apipe::CommandPipe;
 //!
@@ -27,30 +27,33 @@
 use anyhow::{Context, Result};
 use std::process::{Command, Output, Stdio};
 
-
 /// A type representing an annonymous pipe
-pub struct CommandPipe(Vec<Command>);
+pub struct CommandPipe {
+    pipeline: Vec<Command>,
+}
 
 impl CommandPipe {
     /// Create a new empty pipe.
-    /// 
+    ///
     /// ## Example
-    /// 
+    ///
     /// ```rust
     /// # use apipe::CommandPipe;
     /// let mut pipe = CommandPipe::new();
     /// ```
     pub fn new() -> Self {
-        CommandPipe(Vec::new())
+        CommandPipe {
+            pipeline: Vec::new(),
+        }
     }
 
     /// Add a command to the pipe.
-    /// 
+    ///
     /// The command is passed eiter as an absolute path or as a relative path.
     /// For relative paths the PATH is checked.
-    /// 
+    ///
     /// ## Example
-    /// 
+    ///
     /// ```
     /// # use apipe::CommandPipe;
     /// let mut pipe = CommandPipe::new();
@@ -58,24 +61,24 @@ impl CommandPipe {
     /// ```
     pub fn add_command(&mut self, c: &str) -> &mut Self {
         let command = Command::new(c);
-        self.0.push(command);
+        self.pipeline.push(command);
 
         self
     }
 
     /// Add a single arguement to the preceding command in the pipe.
-    /// 
+    ///
     /// Arguments need to be passed one at a time.
-    /// 
+    ///
     /// ## Example
-    /// 
+    ///
     /// ```
     /// # use apipe::CommandPipe;
     /// let mut pipe = CommandPipe::new();
     /// pipe.add_command("ls").arg("-la");
     /// ```
     pub fn arg(&mut self, arg: &str) -> &mut Self {
-        self.0
+        self.pipeline
             .last_mut()
             .expect("No Command in pipe to add args to.")
             .arg(arg);
@@ -83,11 +86,11 @@ impl CommandPipe {
     }
 
     /// Add multiple arguements to the preceding command in the pipe.
-    /// 
+    ///
     /// Arguments are passed as a sequence.
-    /// 
+    ///
     /// ## Example
-    /// 
+    ///
     /// ```
     /// # use apipe::CommandPipe;
     /// let mut pipe = CommandPipe::new();
@@ -98,7 +101,7 @@ impl CommandPipe {
         I: IntoIterator<Item = S>,
         S: AsRef<std::ffi::OsStr>,
     {
-        self.0
+        self.pipeline
             .last_mut()
             .expect("No Command in pipe to add args to.")
             .args(args);
@@ -106,11 +109,9 @@ impl CommandPipe {
     }
 
     /// Runs the commands in the pipe and returns the output.
-    /// 
-    /// The return type is [`std::process::Output`].
-    /// 
+    ///
     /// ## Example
-    /// 
+    ///
     /// ```
     /// # use apipe::CommandPipe;
     /// let mut pipe = CommandPipe::new();
@@ -118,11 +119,11 @@ impl CommandPipe {
     ///     .args(vec!["-la", "~/Documents"])
     ///     .add_command("grep")
     ///     .arg("My_Dir");
-    /// 
+    ///
     /// let output = pipe.spawn();
     /// ```
     pub fn spawn(mut self) -> Result<Output> {
-        let mut commands = self.0.iter_mut();
+        let mut commands = self.pipeline.iter_mut();
         let mut child = commands
             .next()
             .context("The pipe seems to be empty, no Commands to spawn.")?
@@ -161,6 +162,11 @@ impl CommandPipe {
 
         Ok(output)
     }
+
+    // Returns a [Vec] with references to all the commands currently in the pipeline.
+    pub fn get_pipeline(&self) -> Vec<&Command> {
+        self.pipeline.iter().collect()
+    }
 }
 
 #[cfg(test)]
@@ -173,7 +179,7 @@ mod tests {
 
         pipe.add_command("ls").arg("-la").arg("~/Documents");
 
-        let args: Vec<&std::ffi::OsStr> = pipe.0[0].get_args().collect();
+        let args: Vec<&std::ffi::OsStr> = pipe.get_pipeline()[0].get_args().collect();
 
         assert_eq!(args, &["-la", "~/Documents"])
     }
@@ -184,7 +190,7 @@ mod tests {
 
         pipe.add_command("ls").args(vec!["-la", "~/Documents"]);
 
-        let args: Vec<&std::ffi::OsStr> = pipe.0[0].get_args().collect();
+        let args: Vec<&std::ffi::OsStr> = pipe.get_pipeline()[0].get_args().collect();
 
         assert_eq!(args, &["-la", "~/Documents"])
     }
